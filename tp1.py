6ac3a6ceb600cdddd6d1f9c6bc985042896dfd6c
clementriu <clement.riu@eleves.enpc.fr>
2016-10-06 14:33:57 +0200

    Version finale, propre.

--- tp1.py
+++ tp1.py
@@ -4,21 +4,6 @@
 from data import *
 
 
-# groups = {
-#     "1": {"Denmark", "England", "France", "Sweden"},
-#     "2": {"CIS", "Germany", "Netherlands", "Scotland"}
-# }
-#
-# matches = [
-#     ("Sweden", 1, 1, "France"),
-#     ("Denmark", 0, 0, "England"),
-#     ("Netherlands", 1, 0, "Scotland"),
-#     ("CIS", 1, 1, "Germany"),
-#     ("France", 0, 0, "England"),
-#     ("Sweden", 1, 0, "Denmark"),
-# ]
-
-
 # Question 1 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -31,36 +16,34 @@ def rangement_donnees(pays_points):
 
 def creation_tableau_valeur(group_local, match_local):
     """Crée un tableau de tuples de la forme (nombre de points, nombre de buts marques, nombre de buts encaisses, pays).
-    Prend en entrée l'ensemble des pays considérés et le tableau de la liste des matches. Renvoie un tableau de tuples.
+    Prend en entrée l'ensemble des pays considérés et le tableau de la liste des matches. Renvoie un tableau de tableau.
     """
     pays_points = []
 
     for pays in group_local:
-        nb_points = 0
-        nb_buts_marques = 0
-        nb_buts_encaisses = 0
+        donnees_pays = [0, 0, 0, pays]
 
-        for i in range(len(match_local)):
+        for match in match_local:
             # On calcule le nombre de points
-            if pays == match_local[i][0]:
-                if match_local[i][1] > match_local[i][2]:
-                    nb_points += 2
-                if match_local[i][1] == match_local[i][2]:
-                    nb_points += 1
+            if pays == match[0]:
+                if match[1] > match[2]:
+                    donnees_pays[0] += 2
+                elif match[1] == match[2]:
+                    donnees_pays[0] += 1
 
-                nb_buts_marques += match_local[i][1]
-                nb_buts_encaisses += match_local[i][2]
+                donnees_pays[1] += match[1]
+                donnees_pays[2] += match[2]
 
-            if pays == match_local[i][3]:
-                if match_local[i][1] < match_local[i][2]:
-                    nb_points += 2
-                if match_local[i][1] == match_local[i][2]:
-                    nb_points += 1
+            if pays == match[3]:
+                if match[1] < match[2]:
+                    donnees_pays[0] += 2
+                elif match[1] == match[2]:
+                    donnees_pays[0] += 1
 
-                nb_buts_marques += match_local[i][2]
-                nb_buts_encaisses += match_local[i][1]
+                donnees_pays[1] += match[2]
+                donnees_pays[2] += match[1]
 
-        pays_points.append((nb_points, nb_buts_marques, nb_buts_encaisses, pays))
+        pays_points.append(donnees_pays)
 
     return pays_points
 
@@ -68,53 +51,39 @@ def creation_tableau_valeur(group_local, match_local):
 def affichage_classement_question_1(group_local, match_local):
     """Permet d'afficher le classement selon la méthode définie par la fonction rangement_donnees. Prend en entrée
     l'ensemble des pays et le tableau des matchs."""
-    # a mettre en fonction externe
-    # taille_max = 0
-    # for pays in group_local:
-    #     if len(pays) > taille_max:
-    #         taille_max = len(pays)
-
     taille_max = max([len(pays) for pays in group_local])
 
     pays_points = creation_tableau_valeur(group_local, match_local)
 
     pays_points = rangement_donnees(pays_points)
 
-    for i in range(len(pays_points)):
-        taille_locale = taille_max - len(pays_points[i][3])
-        difference_buts = pays_points[i][1] - pays_points[i][2]
+    for points_pays in pays_points:
+        taille_locale = taille_max - len(points_pays[3])
+        difference_buts = points_pays[1] - points_pays[2]
 
-        print(pays_points[i][3] + " " + "." * (taille_locale + 3) + " " + str(pays_points[i][0]) + " pts " + ["", "+"][
-            difference_buts >= 0] + str(difference_buts))
+        print("{0} {1} {2} pts {3}{4}".format(points_pays[3], "." * (taille_locale + 3), str(points_pays[0]),
+                                              ["", "+"][difference_buts >= 0], str(difference_buts)))
 
 
 print("Question 1 :")
 affichage_classement_question_1(groups["1"], matches)
 print("\n")
 
+
 # Question 2 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 def affichage_matches_joues_question_2(group_local, match_local):
     """Affiche les résultats des matchs d'un groupe. Prend en entrée un ensemble de pays et un tableau de matchs."""
-    # a mettre en fonction externe
-    taille_max = 0
-    for i in range(len(match_local)):
-        if match_local[i][0] in group_local or match_local[i][3] in group_local:
-            if len(match_local[i][0]) > taille_max:
-                taille_max = len(match_local[i][0])
-            if len(match_local[i][3]) > taille_max:
-                taille_max = len(match_local[i][3])
 
-    for i in range(len(match_local)):
-        if match_local[i][0] in group_local or match_local[i][3] in group_local:
-            taille_locale = taille_max - len(match_local[i][0])
-
-            print(match_local[i][0] + " " + " " * (taille_locale) + str(match_local[i][1]) + " - " + str(
-                match_local[i][2]) + " " + match_local[i][3])
+    taille_max = max(len(match[0]) for match in match_local if match[0] in group_local)
+    for match in match_local:
+        if match[0] in group_local or match[3] in group_local:
+            print("{: <{width}} {} - {} {}".format(*match, width=taille_max))
 
 
 print("Question 2 :")
 affichage_matches_joues_question_2(groups["1"], matches)
 print("\n")
+
 
 # Question 3 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -126,13 +95,12 @@ def verifie_match(group_local, match_local):
     for pays in group_local:
         group_travail.difference_update({pays})
         for pays2 in group_travail:
-            if pays2 != pays :
-                match_existe = False
-                for match in match_local:
-                    if (pays in match) and (pays2 in match):
-                        match_existe = True
-                if match_existe == False:
-                    match_non_fait.append((pays, pays2))
+            match_existe = False
+            for match in match_local:
+                if (pays in match) and (pays2 in match):
+                    match_existe = True
+            if match_existe == False:
+                match_non_fait.append((pays, pays2))
     return match_non_fait
 
 
@@ -141,13 +109,12 @@ def affiche_match_non_fait_question_3(group_local, match_local):
     match_non_fait = verifie_match(group_local, match_local)
     max_len_car_pays = max(len(match[0]) for match in match_non_fait)
     for match in match_non_fait:
-        print("{0} {1} vs {2}".format(match[0], " " * (max_len_car_pays - len(match[0])), match[1]))
+        print("{0} {1}vs {2}".format(match[0], " " * (max_len_car_pays - len(match[0])), match[1]))
 
 
 print("Question 3 :")
 affiche_match_non_fait_question_3(groups["2"], matches)
 print("\n")
-
 
 # Question 4 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
